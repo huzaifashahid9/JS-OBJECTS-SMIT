0996a10a778fe064841eb1bad068f246ec22a33e
huzaifashahid9 <huzaifashahid991@gmail.com>
2024-10-15 10:52:46 -0700

    object completed till chap 10

---
+++ assignment6.js
@@ -0,0 +1,67 @@
+//question1
+// var a = 10;
+// document.write(`Result <br> The value of a is ${a}<br><br><br><hr>`);
+
+// var a = ++a;
+// document.write(`The value of ++a is ${a}<br> Now the value of a is ${a} <br><br>`);
+
+// var a = a++;
+// document.write(`The value of a++ is ${a}<br> Now the value of a is ${a} <br><br>`);
+
+// var a = --a;
+// document.write(`The value of --a is ${a}<br> Now the value of a is ${a} <br><br>`);
+
+// var a = a--;
+// document.write(`The value of a-- is ${a}<br> Now the value of a is ${a} <br><br>`);
+
+
+//question2
+// var a =2,b=1;
+// var result = --a - --b + ++b + b--;
+// document.write(`Reult is ${result}<br>`);
+// document.write(`--a is ${--a}<br>`);
+// document.write(`--a - --b is ${--a - --b}<br>`);
+// document.write(`--a - --b + ++b is ${--a - --b + ++b}<br>`);
+// document.write(`--a - --b + ++b +b-- is ${--a - --b + ++b +b--}<br>`);
+
+//question3
+// var userName = prompt("Enter Your Name : ");
+// document.write(`Hello ${userName} Welcome to our Clothing Store <br> How can i help you?`);
+
+//question4
+
+// var table = +prompt("Enter Table Number : ");
+// for (i=1;i<11;i++){
+//     document.write(`${table} x ${i} = ${table*i} <br>`);
+// }
+
+//question5
+
+// var firstSubject = prompt("Enter First Subject Name : ");
+// var secondSubject = prompt("Enter second Subject Name : ");
+// var thirdSubject = prompt("Enter third Subject Name : ");
+// var totalMark = 300;
+// var mark1 = +prompt(`Enter Obtained Marks of ${firstSubject} (0-100)`);
+// var mark2 = +prompt(`Enter Obtained Marks of ${secondSubject} (0-100)`);
+// var mark3 = +prompt(`Enter Obtained Marks of ${thirdSubject} (0-100)`);
+// var markSum = mark1+mark2+mark3;
+// var percentage = (markSum/totalMark)*100;
+// document.write(`<h1>Subject TotalMarks ObtainedMarks </h1>`);
+// document.write(`${firstSubject}                     ${totalMark}                              ${mark1} <br>`)
+// document.write(`${secondSubject}                     ${totalMark}                             ${mark2}  <br>`)
+// document.write(`${thirdSubject}                       ${totalMark}                            ${mark3} <br>`)
+// document.write(`                                       ${markSum}                                 ${percentage} <br>`)
+
+
+//question6
+
+
+
+
+
+
+
+
+
+
+
---
+++ assignment7.js
@@ -0,0 +1,152 @@
+//question1
+// var userCity = prompt("Enter Your City : ");
+// if(userCity == "karachi" ){
+//     document.write(`Welcome to city of lights `);
+// }else{
+//     document.write(`Not welcome`);
+// }
+
+//question2
+// var gender = prompt('Enter your gender : ');
+// if(gender == "male"){
+//     document.write(`Good Morning Sir!`);
+// }else if(gender == "female"){
+//     document.write(`Good Morning Mam!`);
+// }else{
+//     document.write(`Good Morning!`);
+// }
+
+//question3
+// var color = prompt("Enter your color (Red,Green,Yello) : ");
+// if(color == "red"){
+//     document.write("Must Stop!");
+// }else if(color == "yello"){
+//     document.write("Ready to Move!");
+// }else{
+//     document.write("Move Now!");
+// }
+
+//question4
+// var fuel = prompt("Enter Your Fuel in Litre : ");
+// if(fuel < 0.25){
+//     document.write("Please Refill the fuel in your car : ");
+// }else{
+//     document.write("Save Journey");
+// }
+
+//question5
+// var car = car;
+// var cat = "cat";
+//  if(car<cat){
+//     alert("car is smaller than cat : ");
+//  }else{
+//     alert("Error : ");
+//  }
+
+//question6
+// document.write(`<h1>Mark Sheet</h1><br><br><br>`);
+// var obtainMarks = +prompt("Enter Your Marks (0-300) : ");
+// var totalMarks = 300;
+// var percent = (obtainMarks/totalMarks)*100;
+// if(percent >= 80){
+//     document.write(`Total Marks = ${totalMarks} <br>`);
+//     document.write(`Obtained Marks = ${obtainMarks} <br>`);
+//     document.write(`Percentage = ${percent} <br>`);
+//     document.write(`Grade : A-one <br>`);
+//     document.write(`ReMarks : Excellent <br>`);
+// }else if(percent >= 70){
+//     document.write(`Total Marks = ${totalMarks} <br>`);
+//     document.write(`Obtained Marks = ${obtainMarks} <br>`);
+//     document.write(`Percentage = ${percent} <br>`);
+//     document.write(`Grade : A <br>`);
+//     document.write(`ReMarks : Good <br>`);
+// }else if( percent >= 60){
+//     document.write(`Total Marks = ${totalMarks} <br>`);
+//     document.write(`Obtained Marks = ${obtainMarks} <br>`);
+//     document.write(`Percentage = ${percent} <br>`);
+//     document.write(`Grade : B <br>`);
+//     document.write(`ReMarks : You need to improve <br>`);
+// }else{
+//     document.write(`Total Marks = ${totalMarks} <br>`);
+//     document.write(`Obtained Marks = ${obtainMarks} <br>`);
+//     document.write(`Percentage = ${percent} <br>`);
+//     document.write(`Grade : Fail <br>`);
+//     document.write(`ReMarks : Sorry <br>`);
+// }
+
+
+//question7
+// var randomNum = Math.floor(Math.random()*10+1);//generate random num from 1 to 10
+// var guessNum = prompt("Guess a Number (1 to 10) : ");
+// // console.log(randomNum);
+// if(randomNum == guessNum){
+//     document.write("Bingo! Correct Answer:");
+// }else if(guessNum>10){
+//     document.write("Invalid Number : ");
+// }else{
+//     document.write(`Close Enough to Correct Answer <br> Correct Answer is ${randomNum}`);
+// }
+
+
+//question8
+// var num = +prompt("Enter Your Number : ");
+// if(num%3==0){
+//     document.write(`${num} is Divisible By 3`);
+// }else{
+//     document.write(`${num} is Not Divisible By 3`);
+// }
+
+//question9
+// var num = +prompt("Enter a Number : ");
+// if(num%2==0){
+//     document.write(`${num} is Even`);
+// }else{
+//     document.write(`${num} is Odd`);
+// }
+
+
+//question10
+// var temp = +prompt("Enter Temperature : ");
+// if(temp > 40){
+//     document.write(`<h3>It is too Hot Outside</h3>`);
+// }else if(temp > 30){
+//     document.write(`<h3>The Weather Today Is Normal/h3>`);
+// }else if(temp > 20){
+//     document.write(`<h3>Todays Weather is Cool</h3>`);
+// }else if(temp > 10){
+//     document.write(`<h3>OMG! Todays Weather is So Cool</h3>`);
+// }else{
+//     document.write(`<h3>It is too Chilled Outside</h3>`);
+// }
+
+//question11
+// var firstNum = +prompt("Enter Number 1 : ");
+// var secondNum = +prompt("Enter Number 1 : ");
+// var operation = prompt("Enter Your operation (+,-,*,/) : ");
+// if(operation == '+'){
+//     document.write(`The Sum of ${firstNum} and ${secondNum} is ${firstNum+secondNum}`);
+// }else if(operation == '-'){
+//     document.write(`The Differnce of ${firstNum} and ${secondNum} is ${firstNum-secondNum}`);
+// }else if(operation == '*'){
+//     document.write(`The Multiplication of ${firstNum} and ${secondNum} is ${firstNum*secondNum}`);
+// }else if(operation == '/'){
+//     document.write(`The Division of ${firstNum} and ${secondNum} is ${firstNum/secondNum}`);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
